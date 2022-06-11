714042bfe07d8a643a64e8ace56ba332ab5e81cf
bijuacharju <bijuacharju4@gmail.com>
2022-06-11 21:48:01 +0545

    Project Completed

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>&copy; 2022 Airbnb Clone! No rights reserved. This is only a sample!</p>
+            <p>Privacy &sdot; Terms &sdot; Sitemap &sdot; Company Details</p>
+        </div>
+    );
+}
+
+export default Footer;
